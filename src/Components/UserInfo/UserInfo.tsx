b801fdbed7d0bc625a38037eac70443f32ccc766
ARTALEAL <artaleal@yandex.ru>
2023-12-01 23:05:59 +0300

    InfoItem

---
+++ src/Components/UserInfo/UserInfo.tsx
@@ -0,0 +1,7 @@
+import styles from './UserInfo.module.scss';
+
+interface UserInfoProps {}
+
+export const UserInfo = ({}: UserInfoProps) => (
+  <div className={styles.userInfo}>UserInfo Component</div>
+);
