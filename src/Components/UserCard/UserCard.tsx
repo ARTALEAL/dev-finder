2dfe7e8c0393179051a36e38e603aa5ba073edfb
ARTALEAL <artaleal@yandex.ru>
2023-12-01 22:46:13 +0300

    title

--- src/Components/UserCard/UserCard.tsx
+++ src/Components/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserStat } from '../UserStat';
+import { UserTitle } from '../UserTitle';
 import { LocalGitHubUser } from '../types';
 import styles from './UserCard.module.scss';
 
@@ -6,6 +7,11 @@ interface UserCardProps extends LocalGitHubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img src={props.avatar} alt="User Avatar" className={styles.avatar} />
+    <UserTitle name={props.name} login={props.login} created={props.created} />
+    <p className={`${styles.bio} ${props.bio ? '' : `${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio'}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
